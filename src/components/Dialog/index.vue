a2f7d83862eeadf2eb3ff7d5a99f276441bca157
moselikk <moselikk@gmail.com>
2022-09-19 15:04:59 +0800

    feat: add info

--- src/components/Dialog/index.vue
+++ src/components/Dialog/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="dialog" @scroll.stop.prevent>
     <img :src="amiiboInfo.image" :alt="amiiboInfo.name">
+    <p>{{amiiboInfo.name}}</p>
+    <p>gameSeries: {{amiiboInfo.gameSeries}}</p>
+    <p>type: {{amiiboInfo.type}}</p>
+    <ul>
+      <p>release</p>
+      <li v-for="(_, $index) in amiiboInfo.release" :key="$index">{{$index}}: {{amiiboInfo.release[$index]}}</li>
+    </ul>
   </div>
 </template>
 
@@ -15,12 +22,20 @@
 
 <style lang="scss" scoped>
 .dialog{
-  width: 300px;
-  height: 500px;
+  padding:30px;
   position:fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   background-color: #bfa;
+  border-radius: 30px;
+  img{
+    height: 80%;
+  }
+  ul{
+    li{
+      list-style: none;
+    }
+  }
 }
 </style>
