f9bf2584fa65823182dc07012150676835aa5566
Moselikk <moselikk@gmail.com>
2023-01-09 11:21:20 +0800

    fix: request erro

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,22 +1,22 @@
 import axios from "axios";
 
 const service = axios.create({
-  baseURL: 'http://121.5.153.162:5000/api',
+  baseURL: 'https://www.amiiboapi.com/api/',
   timeout: 5000
 })
 
 service.interceptors.response.use(response => {
   const res = response.data
-  if(response.status == 200){
+  if (response.status == 200) {
     return res;
   } else {
     alert('出错了')
   }
 },
-error => {
-  console.log('err' + error)
-  return Promise.reject(error)
-}
+  error => {
+    console.log('err' + error)
+    return Promise.reject(error)
+  }
 )
 
 export default service
