044f8b351948d53467e5e3706ae88166f2de3392
Moselikk <moselikk@gmail.com>
2023-01-12 13:13:03 +0800

    feat: add github workflows

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
@@ -17,5 +21,26 @@ export default defineConfig({
     port: 2002,
     cors: true, // 跨域设置允许
     strictPort: true, // 如果端口已占用直接退出
+  },
+  build: {
+    reportCompressedSize: false,
+    chunkSizeWarningLimit: 2000,
+    minify: "esbuild",
+    assetsDir: "static/assets",
+    rollupOptions: {
+      output: {
+        sanitizeFileName(fileName) {
+          const match = DRIVE_LETTER_REGEX.exec(fileName);
+          const driveLetter = match ? match[0] : "";
+          return (
+            driveLetter +
+            fileName.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+          );
+        },
+        chunkFileNames: "static/js/[name]-[hash].js",
+        entryFileNames: "static/js/[name]-[hash].js",
+        assetFileNames: "static/[ext]/[name]-[hash].[ext]",
+      },
+    },
   },
 });
